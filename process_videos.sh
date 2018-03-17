b74c9cc05e8893addfbfda2cd167083e8eaedc95
Michał Narajowski <michal.narajowski@codecoup.pl>
2018-03-17 18:00:35 +0100

    Save also the video file when using process_videos script

--- process_videos.sh
+++ process_videos.sh
@@ -13,5 +13,6 @@ for filepath in $videos_list; do
         remote_video_path="$remote_video_dir/$filename"
         cmd="${yolo_cmd} $remote_video_path"
         output_logs_file="${filename}_yolo.csv"
-        ssh $remote_addr "cd $remote_video_dir; ls -la $filename; cd $darknet_path; pwd; $cmd; ls -la logs.txt; mv logs.txt $output_logs_file; rm $remote_video_path; ls -la $remote_video_path"
+        output_video_file="${filename}_yolo.avi"
+        ssh $remote_addr "cd $remote_video_dir; ls -la $filename; cd $darknet_path; pwd; $cmd; ls -la logs.txt; mv logs.txt $output_logs_file; ls -la test_dnn_out.avi; mv test_dnn_out.avi $output_video_file; rm $remote_video_path; ls -la $remote_video_path"
 done
